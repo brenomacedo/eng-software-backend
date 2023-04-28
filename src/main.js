4567507a23124c8c1585f4c5cc749e64d0acdbde
Breno Macêdo <b88660505@gmail.com>
2023-04-27 21:40:43 -0300

    implementing error handling

--- src/main.js
+++ src/main.js
@@ -1,6 +1,8 @@
 import express from 'express';
-import './database/index.js';
 import userRoutes from './routes/userRoutes.js';
+import { errorHandler } from './utils/errors.js';
+import 'express-async-errors';
+import './database/index.js';
 
 const API_ENTRYPOINT = '/api/v1';
 const app = express();
@@ -9,6 +11,8 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.use(API_ENTRYPOINT, userRoutes);
+
+app.use(errorHandler);
 
 app.listen(3333, () => {
   console.log('App listening on port 3333');
