d314bcbf9ef612c23584998eacdf6f8760e53af6
Breno Macêdo <b88660501@gmail.com>
2023-06-24 21:20:27 -0300

    removing auth verification from index nearest events

--- src/routes/eventRoutes.js
+++ src/routes/eventRoutes.js
@@ -6,7 +6,7 @@ const router = new Router();
 
 router.post('/event', auth, eventController.create);
 router.get('/event/me', auth, eventController.indexUserEvents);
-router.get('/event', auth, eventController.indexNearestEvents);
+router.get('/event', eventController.indexNearestEvents);
 router.patch('/event/:id', auth, eventController.update);
 router.delete('/event/:id', auth, eventController.delete);
 
