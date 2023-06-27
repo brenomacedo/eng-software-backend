5876dc924c690ad54ad4d030cc5549e24c18e83a
Breno Macêdo <b88660501@gmail.com>
2023-06-27 20:26:58 -0300

    debugging env error

--- src/database/index.js
+++ src/database/index.js
@@ -7,6 +7,9 @@ if (process.env.NODE_ENV !== 'production') {
   dotenv.config();
 }
 
+console.log('node_env: ', process.env.NODE_ENV);
+console.log('config: ', config[process.env.NODE_ENV || 'development']);
+
 const knex = Knex(config[process.env.NODE_ENV || 'development']);
 
 Model.knex(knex);
