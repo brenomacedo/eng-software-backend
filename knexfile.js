86b88e3c6ceb7a674d6a326d72f6aec0840af9ec
Breno Macêdo <b88660501@gmail.com>
2023-06-27 20:24:03 -0300

    adding production to knexfile

--- knexfile.js
+++ knexfile.js
@@ -27,5 +27,24 @@ export default {
     seeds: {
       directory: './src/database/seeds'
     }
+  },
+  production: {
+    client: 'pg',
+
+    connection: {
+      host: process.env.DB_HOST,
+      port: process.env.DB_PORT,
+      user: process.env.DB_USER,
+      database: process.env.DB_DATABASE,
+      password: process.env.DB_PASSWORD,
+      ssl: { rejectUnauthorized: false }
+    },
+    migrations: {
+      tableName: 'knex_migrations',
+      directory: './src/database/migrations'
+    },
+    seeds: {
+      directory: './src/database/seeds'
+    }
   }
 };
