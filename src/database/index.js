b7d674f8c5ae416be51a393818da0903480404bd
Breno Macêdo <b88660501@gmail.com>
2023-06-27 20:29:41 -0300

    production

--- src/database/index.js
+++ src/database/index.js
@@ -7,9 +7,6 @@ if (process.env.NODE_ENV !== 'production') {
   dotenv.config();
 }
 
-console.log('node_env: ', process.env.NODE_ENV);
-console.log('config: ', config[process.env.NODE_ENV || 'development']);
-
 const knex = Knex(config[process.env.NODE_ENV || 'development']);
 
 Model.knex(knex);
